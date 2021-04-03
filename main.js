c03e10c497b9a252916139dd4dbe9491d0be121c
Jack Misteli <jgmisteli@gmail.com>
2021-04-03 13:00:37 +1100

    IOT animation

--- main.js
+++ main.js
@@ -1325,14 +1325,10 @@
         '#f725a0',
         '#fad141',
         '#0cb2c0',
-        '#e8e6d9',
-        '#000'
+        '#d27dff',
     ];
     var getRandomColor = function () {
         return colorsHex[Math.floor(Math.random() * colorsHex.length)];
-    };
-    var getRandomColorMinusBlack = function () {
-        return colorsHex[Math.floor(Math.random() * (colorsHex.length - 1))];
     };
 
     var lastScrollTop = 0;
@@ -1409,7 +1405,7 @@
             var sections = document.getElementsByClassName('section');
             for (var ii = 0; ii < sections.length; ii++) {
                 var section = sections[ii];
-                var text = texts[ii][i] || texts[0];
+                var text = texts[ii][i] || texts[0].join(' ');
                 // registerSectionObserver(section, cubes, text)
                 var collide = areColliding(section, cube);
                 if (collide) {
@@ -1711,6 +1707,7 @@
                         var triCol = colorsHex[Math.floor(Math.random() * 6)];
                         if (triCol !== rectCol && triCol !== circCol) {
                             ctx.fillStyle = triCol;
+                            //@ts-ignore
                             ctx.fillOpacity = Math.random() * 0.3;
                             ctx.fill();
                             ctx.closePath();
@@ -1721,171 +1718,9 @@
         }
     };
 
-    // Thanks to https://gist.github.com/banksean/304522
-    // Ported from Stefan Gustavson's java implementation
-    // http://staffwww.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
-    // Read Stefan's excellent paper for details on how this code works.
-    //
-    // Sean McCullough banksean@gmail.com
-    /**
-     * You can pass in a random number generator object if you like.
-     * It is assumed to have a random() method.
-     */
-    var ClassicalNoise = function (r) {
-        if (r == undefined)
-            r = Math;
-        this.grad3 = [[1, 1, 0], [-1, 1, 0], [1, -1, 0], [-1, -1, 0],
-            [1, 0, 1], [-1, 0, 1], [1, 0, -1], [-1, 0, -1],
-            [0, 1, 1], [0, -1, 1], [0, 1, -1], [0, -1, -1]];
-        this.p = [];
-        for (var i = 0; i < 256; i++) {
-            this.p[i] = Math.floor(r.random() * 256);
-        }
-        // To remove the need for index wrapping, double the permutation table length 
-        this.perm = [];
-        for (var i = 0; i < 512; i++) {
-            this.perm[i] = this.p[i & 255];
-        }
-    };
-    ClassicalNoise.prototype.dot = function (g, x, y, z) {
-        return g[0] * x + g[1] * y + g[2] * z;
-    };
-    ClassicalNoise.prototype.mix = function (a, b, t) {
-        return (1.0 - t) * a + t * b;
-    };
-    ClassicalNoise.prototype.fade = function (t) {
-        return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
-    };
-    // Classic Perlin noise, 3D version 
-    ClassicalNoise.prototype.noise = function (x, y, z) {
-        // Find unit grid cell containing point 
-        var X = Math.floor(x);
-        var Y = Math.floor(y);
-        var Z = Math.floor(z);
-        // Get relative xyz coordinates of point within that cell 
-        x = x - X;
-        y = y - Y;
-        z = z - Z;
-        // Wrap the integer cells at 255 (smaller integer period can be introduced here) 
-        X = X & 255;
-        Y = Y & 255;
-        Z = Z & 255;
-        // Calculate a set of eight hashed gradient indices 
-        var gi000 = this.perm[X + this.perm[Y + this.perm[Z]]] % 12;
-        var gi001 = this.perm[X + this.perm[Y + this.perm[Z + 1]]] % 12;
-        var gi010 = this.perm[X + this.perm[Y + 1 + this.perm[Z]]] % 12;
-        var gi011 = this.perm[X + this.perm[Y + 1 + this.perm[Z + 1]]] % 12;
-        var gi100 = this.perm[X + 1 + this.perm[Y + this.perm[Z]]] % 12;
-        var gi101 = this.perm[X + 1 + this.perm[Y + this.perm[Z + 1]]] % 12;
-        var gi110 = this.perm[X + 1 + this.perm[Y + 1 + this.perm[Z]]] % 12;
-        var gi111 = this.perm[X + 1 + this.perm[Y + 1 + this.perm[Z + 1]]] % 12;
-        // The gradients of each corner are now: 
-        // g000 = grad3[gi000]; 
-        // g001 = grad3[gi001]; 
-        // g010 = grad3[gi010]; 
-        // g011 = grad3[gi011]; 
-        // g100 = grad3[gi100]; 
-        // g101 = grad3[gi101]; 
-        // g110 = grad3[gi110]; 
-        // g111 = grad3[gi111]; 
-        // Calculate noise contributions from each of the eight corners 
-        var n000 = this.dot(this.grad3[gi000], x, y, z);
-        var n100 = this.dot(this.grad3[gi100], x - 1, y, z);
-        var n010 = this.dot(this.grad3[gi010], x, y - 1, z);
-        var n110 = this.dot(this.grad3[gi110], x - 1, y - 1, z);
-        var n001 = this.dot(this.grad3[gi001], x, y, z - 1);
-        var n101 = this.dot(this.grad3[gi101], x - 1, y, z - 1);
-        var n011 = this.dot(this.grad3[gi011], x, y - 1, z - 1);
-        var n111 = this.dot(this.grad3[gi111], x - 1, y - 1, z - 1);
-        // Compute the fade curve value for each of x, y, z 
-        var u = this.fade(x);
-        var v = this.fade(y);
-        var w = this.fade(z);
-        // Interpolate along x the contributions from each of the corners 
-        var nx00 = this.mix(n000, n100, u);
-        var nx01 = this.mix(n001, n101, u);
-        var nx10 = this.mix(n010, n110, u);
-        var nx11 = this.mix(n011, n111, u);
-        // Interpolate the four results along y 
-        var nxy0 = this.mix(nx00, nx10, v);
-        var nxy1 = this.mix(nx01, nx11, v);
-        // Interpolate the two last results along z 
-        var nxyz = this.mix(nxy0, nxy1, w);
-        return nxyz;
-    };
-
-    var scale = function (num, in_min, in_max, out_min, out_max) {
-        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    };
-    var MyCircle = /** @class */ (function () {
-        // canvasOffset: number;
-        function MyCircle(ctx, x, y, r, color) {
-            this.x = x;
-            this.y = y;
-            this.maxR = r;
-            this.perlinSeed = 1;
-            this.ctx = ctx;
-            this.perlinNoise = new ClassicalNoise({ random: function () { return Math.random(); } });
-            this.color = color;
-            this.stretch = Math.random() * 10 + 5;
-        }
-        MyCircle.prototype.updateSeed = function (addValue) {
-            // const {perlinSeed} = this
-            this.perlinSeed = this.perlinSeed + Math.random();
-            // const val = this.perlinSeed
-            // this.perlinNoise = new  perlin({random:()=>Math.sin(val)})
-        };
-        MyCircle.prototype.distort = function () {
-        };
-        MyCircle.prototype.draw = function (mouseX, mouseY) {
-            var _a = this, x = _a.x, y = _a.y, maxR = _a.maxR, perlinSeed = _a.perlinSeed, ctx = _a.ctx;
-            // const p = perlin(perlinSeed)
-            // console.log('perlin seed', perlinSeed)
-            // console.log()
-            // console.log(perlinSeed)
-            // ctx.beginPath();
-            // ctx.moveTo (60, 10);
-            // ctx.lineTo (60, 110);
-            // ctx.bezierCurveTo (0, 110, 0, 10, 60, 10);
-            // ctx.fill();
-            // for (let i = 0; i < Math.PI)
-            var cos = Math.cos, sin = Math.sin, PI = Math.PI;
-            ctx.fillStyle = this.color;
-            var scope = 150;
-            var radius;
-            // if(!inscope) {
-            //     radius = 0 
-            // } else {
-            var distanceFromMouse = Math.abs(Math.sqrt(Math.pow(x - mouseX, 2) + Math.pow(y - mouseY, 2)));
-            console.log({ distanceFromMouse: distanceFromMouse });
-            radius = scale(distanceFromMouse, scope, 0, maxR, 0);
-            // }
-            // const mousePos = 
-            var i = 0;
-            // let movingmaxR = r
-            ctx.beginPath();
-            // ctx.moveTo(x, y);
-            while (i < PI * 2) {
-                // console.log(randn_bm())
-                // // const h = noise.dot()
-                var thisX = cos(i) * radius + x;
-                var thisY = sin(i) * radius + y;
-                var thisN = this.perlinNoise.noise(thisX, thisY, this.perlinSeed);
-                // console.log(thisN)
-                // movingR += randn_bm()
-                ctx.lineTo(thisX + thisN * this.stretch, thisY + thisN * this.stretch);
-                i += 0.01;
-            }
-            ctx.fill();
-        };
-        return MyCircle;
-    }());
-
     var allBlobs = [];
     var canvasHeight = window.innerHeight;
     var canvasWidth = window.innerWidth * 4;
-    var blobMargin = 100;
-    var blobSize = 60;
     var canvasOffsetX = 0;
     var canvasOffsetY = 0;
     var background;
@@ -1909,11 +1744,11 @@
             }
         };
         // mainBlob =  new MyBlob (ctx, 130,130, 50, getRandomColor())
-        for (var y = 0; y < canvasWidth / (blobSize / blobMargin); y += blobSize + blobMargin) {
-            for (var x = 0; x < canvasHeight / (blobSize / blobMargin); x += blobSize + blobMargin) {
-                allBlobs.push(new MyCircle(ctx, x + blobSize + blobMargin, y + blobSize + blobMargin, blobSize, 'rgba(255, 255, 255, 0.4)', getRandomColorMinusBlack()));
-            }
-        }
+        // for (let y = 0; y < canvasWidth / (blobSize/blobMargin); y+=blobSize + blobMargin) {
+        //     for (let x = 0; x < canvasHeight / (blobSize/blobMargin); x+=blobSize + blobMargin) {
+        //         allBlobs.push(new MyBlob(ctx, x+blobSize + blobMargin, y+blobSize + blobMargin, blobSize, 'rgba(255, 255, 255, 0.4)', getRandomColorMinusBlack()))
+        //     }
+        // }
         draw();
         window.onmousemove = function (e) {
             var pageX = e.pageX, pageY = e.pageY;
@@ -1944,7 +1779,46 @@
         createCanvas$1();
     });
 
+    var isInViewPort = function (element) {
+        var rect = element.getBoundingClientRect();
+        var html = document.documentElement;
+        return (rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth));
+    };
+
+    var highlightText = function () {
+        var highlitables = document.getElementsByClassName('highlight');
+        function highlight(target, i) {
+            if (isInViewPort(target)) {
+                console.log("IN VIEWPORT!");
+                // var background = document.createElement('div')
+                // background.className = 'highlight-background'
+                // background.style.background = themeColors[i%themeColors.length]
+                // target.style.position = 'relative'
+                // target.style.zIndex = '1'
+                // target.appendChild(background)
+                // if (target.classList.i)
+                window.setTimeout(function () {
+                    if (!target.classList[1]) {
+                        var backgroundColor = colorsHex[Math.floor(Math.random() * colorsHex.length)];
+                        var backgroundStyle = 'linear-gradient(to right, ' +
+                            backgroundColor + ',' +
+                            backgroundColor + ' 50%, transparent 50%, transparent)';
+                        target.style.backgroundImage = backgroundStyle;
+                        target.classList.add('show');
+                    }
+                }, Math.random() * 4000);
+            }
+        }
+        for (var i = 0; i < highlitables.length; i++) {
+            highlight(highlitables[i]);
+        }
+    };
+
     docReady(function () {
+        highlightText();
         // anime({
         //     targets: 'h1',
         //     // translateX: 250,
